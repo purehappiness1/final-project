d9307d9d47cac9a704cb5d160414dc771818166d
Alex Shostka <burda3000@gmail.com>
2020-08-06 14:44:25 +0300

    changes

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,45 +1,41 @@
-
-
 export const sendSignup = (firstName, lastName, email, password) => async (dispatch) => {
-  console.log(firstName, lastName, email, password)
+  console.log(firstName, lastName, email, password);
   try {
-    const response = await fetch(`http://localhost:3100/signup`, {
-      method: 'POST',
+    await fetch(`http://localhost:3100/signup`, {
+      method: "POST",
       body: JSON.stringify({
         firstName: firstName,
         lastName: lastName,
         email: email,
-        password: password
+        password: password,
       }),
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-  }) 
-  //const edited = await response.json();
+    });
+    //const edited = await response.json();
   } catch (err) {
-    console.log("Err", err)
+    console.log("Err", err);
   }
-  
-}
+};
 
 export const sendLogin = (email, password) => async (dispatch) => {
-  console.log(email, password)
+  console.log(email, password);
   try {
-    const response = await fetch(`http://localhost:3100/login`, {
-      method: 'POST',
+    await fetch(`http://localhost:3100/login`, {
+      method: "POST",
       body: JSON.stringify({
         email: email,
-        password: password
+        password: password,
       }),
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-  }) 
-  //const edited = await response.json();
+    });
+    //const edited = await response.json();
   } catch (err) {
-    console.log("Err", err)
+    console.log("Err", err);
   }
-  
-}
+};
