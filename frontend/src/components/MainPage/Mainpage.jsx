690afb5f699e9ded8e3424cc504f811de7954a3f
Alex Shostka <burda3000@gmail.com>
2020-08-12 12:09:21 +0300

    landing page

--- frontend/src/components/MainPage/Mainpage.jsx
+++ frontend/src/components/MainPage/Mainpage.jsx
@@ -1,46 +1,39 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardHeader from '@material-ui/core/CardHeader';
-import CssBaseline from '@material-ui/core/CssBaseline';
-// import Grid from '@material-ui/core/Grid';
-// import StarIcon from '@material-ui/icons/StarBorder';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Все права защищены © '}
+      {"Все права защищены © "}
       <Link color="inherit" href="https://material-ui.com/">
         Beta CRM
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
-  '@global': {
+  "@global": {
     ul: {
       margin: 0,
       padding: 0,
-      listStyle: 'none',
+      listStyle: "none",
     },
   },
   appBar: {
     borderBottom: `1px solid ${theme.palette.divider}`,
   },
   toolbar: {
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
   },
   toolbarTitle: {
     flexGrow: 1,
@@ -48,83 +41,72 @@ const useStyles = makeStyles((theme) => ({
   link: {
     margin: theme.spacing(1, 1.5),
   },
-  heroContent: {
-    padding: theme.spacing(8, 0, 6),
-  },
   cardHeader: {
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
   },
   cardPricing: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'baseline',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
     marginBottom: theme.spacing(2),
   },
   footer: {
     borderTop: `1px solid ${theme.palette.divider}`,
-    marginTop: theme.spacing(8),
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-    [theme.breakpoints.up('sm')]: {
-      paddingTop: theme.spacing(6),
-      paddingBottom: theme.spacing(6),
-    },
+    // marginTop: theme.spacing(8),
+    // paddingTop: theme.spacing(3),
+    // paddingBottom: theme.spacing(3),
+    // [theme.breakpoints.up('sm')]: {
+    // paddingTop: theme.spacing(6),
+    // paddingBottom: theme.spacing(6),
+    // },
+  },
+  test: {
+    border: "1px solid red",
+    borderRadius: "5px",
+  },
+
+  all: {
+    padding: 0,
+    margin: 0,
+    boxSizing: "border-box",
+    background: "#eee",
+  },
+  bannerArea: {
+    width: "100%",
+    height: "100vh",
+    background: 'url("https://unsplash.it/1300/600?random")',
+    backgroundSize: "cover",
+  },
+  contentArea: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    textAlign: "center",
+  },
+  h1: {
+    fontSize: "60px",
+    color: "#fff",
+  },
+  container: {
+    width: "70%",
+    margin: "50px auto",
+    padding: "10px",
+  },
+  box: {
+    width: "100%",
+    padding: "50px",
+    background: "#fff",
+    marginTop: "50px",
+    borderRadius: "10px",
+    boxShadow: "0 0 10px -3px rgba(0,0,0 .1)",
   },
 }));
-
-// const tiers = [
-//   {
-//     title: 'Бесплатно',
-//     price: '0',
-//     description: ['10 пользователей включено', '2 ГБ хранилища', 'Help center access', 'Email support'],
-//     buttonText: 'Sign up for free',
-//     buttonVariant: 'outlined',
-//   },
-//   {
-//     title: 'Про',
-//     subheader: 'Most popular',
-//     price: '15',
-//     description: [
-//       '20 users included',
-//       '10 GB of storage',
-//       'Help center access',
-//       'Priority email support',
-//     ],
-//     buttonText: 'Get started',
-//     buttonVariant: 'contained',
-//   },
-//   {
-//     title: 'Enterprise',
-//     price: '30',
-//     description: [
-//       '50 users included',
-//       '30 GB of storage',
-//       'Help center access',
-//       'Phone & email support',
-//     ],
-//     buttonText: 'Contact us',
-//     buttonVariant: 'outlined',
-//   },
-// ];
-// const footers = [
-//   {
-//     title: 'Company',
-//     description: ['Team', 'History', 'Contact us', 'Locations'],
-//   },
-//   {
-//     title: 'Features',
-//     description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
-//   },
-//   {
-//     title: 'Resources',
-//     description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-//   },
-//   {
-//     title: 'Legal',
-//     description: ['Privacy policy', 'Terms of use'],
-//   },
-// ];
 
 export default function Pricing() {
   const classes = useStyles();
@@ -132,68 +114,98 @@ export default function Pricing() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-            Beta CRM
-          </Typography>
-          <Button href="/register" color="primary" variant="outlined" className={classes.link}>Register</Button>
-          <Button href="/signin" color="primary" variant="outlined" className={classes.link}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
-      {/* Hero unit */}
-      <Container maxWidth="sm" component="main" className={classes.heroContent}>
-        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-         Добро пожаловать
-        </Typography>
-        <Typography variant="h5" align="center" color="textSecondary" component="p">
-          Возможность удобного управления бизнесом со всеми нужными вам инструментами
-        </Typography>
-      </Container>
-      {/* End hero unit */}
-      {/* <Container maxWidth="md" component="main">
-        <Grid container spacing={5} alignItems="flex-end">
-          {tiers.map((tier) => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
-              <Card>
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  subheaderTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
-                  className={classes.cardHeader}
-                />
-                <CardContent>
-                  <div className={classes.cardPricing}>
-                    <Typography component="h2" variant="h3" color="textPrimary">
-                      ${tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="textSecondary">
-                      /mo
-                    </Typography>
-                  </div>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography component="li" variant="subtitle1" align="center" key={line}>
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-                <CardActions>
-                  <Button fullWidth variant={tier.buttonVariant} color="primary">
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container> */}
+      <div className={classes.all}>
+        <div className={classes.bannerArea}>
+          <div className={classes.contentArea}>
+            <div className={classes.content}>
+              <h1 className={classes.h1}>Ваш учебный центр растёт?</h1>
+              <h3>Не теряйте клиентов и увеличивайте доход</h3>
+              <ul>
+                <li>
+                  Новая воронка продаж позволит сохранить и увеличить приток
+                  клиентов;
+                </li>
+                <li>
+                  Система сама рассчитает абонементы и снимет боль о платежах и
+                  долгах;
+                </li>
+                <li>
+                  Автоматические SMS и E-mail рассылки повысят посещаемость и
+                  выручку;
+                </li>
+                <li>
+                  Служба поддержки поможет настроить и адаптировать CRM под вас;
+                </li>
+              </ul>
+              <Button
+                href="/register"
+                color="primary"
+                variant="outlined"
+                className={classes.content}
+              >
+                Register
+              </Button>
+              <Button
+                href="/signin"
+                color="primary"
+                variant="outlined"
+                className={classes.content}
+              >
+                Login
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        {/* main part */}
+
+        <div className={classes.container}>
+          <div className={classes.box}>
+            <h3>Поддержка и помощь на всех этапах</h3>
+            <p>
+              CRM — одна из самых дружелюбных CRM для внедрения: на каждом этапе
+              вам будут помогать наши специалисты из отдела поддержки. Мы
+              поможем настроить воронку продаж с интеграциями, автоматические и
+              триггерные рассылки, дополнительные поля, виджеты онлайн записи,
+              кастомные отчеты и другие важные параметры системы. Для обращения
+              в поддержку достаточно написать в ЧАТ справа снизу (два облачка).
+              Можно попросить, чтобы вам перезвонили, или описать проблему в
+              чате.
+            </p>
+          </div>
+        </div>
+
+        <div className={classes.container}>
+          <div className={classes.box}>
+            <h3>Поддержка и помощь на всех этапах</h3>
+            <p>
+              CRM — одна из самых дружелюбных CRM для внедрения: на каждом этапе
+              вам будут помогать наши специалисты из отдела поддержки. Мы
+              поможем настроить воронку продаж с интеграциями, автоматические и
+              триггерные рассылки, дополнительные поля, виджеты онлайн записи,
+              кастомные отчеты и другие важные параметры системы. Для обращения
+              в поддержку достаточно написать в ЧАТ справа снизу (два облачка).
+              Можно попросить, чтобы вам перезвонили, или описать проблему в
+              чате.
+            </p>
+          </div>
+        </div>
+
+        <div className={classes.container}>
+          <div className={classes.box}>
+            <h3>Интерактивный календарь</h3>
+            <p>
+              Календарь — основной инструмент работы учебного центра. Всё как на
+              ладони: занятые и свободные аудитории, уроки, педагоги, пробные,
+              отмены, отработки. При беглом взгляде на календарь всегда можно
+              понять кто придёт на урок, у кого из них заканчивается оплата.
+              Можно легко отфильтровать по разным критериям, изменить тип
+              отображения или состав карточки.
+            </p>
+          </div>
+        </div>
+      </div>
+
       {/* Footer */}
       <Container maxWidth="md" component="footer" className={classes.footer}>
         <Box mt={5}>
