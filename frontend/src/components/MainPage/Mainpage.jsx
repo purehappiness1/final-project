4fb86a77a3fa910570f293f79c6da060d5ffe3dc
Alex Shostka <burda3000@gmail.com>
2020-08-12 17:47:14 +0300

    new parts of mainpage

--- frontend/src/components/MainPage/Mainpage.jsx
+++ frontend/src/components/MainPage/Mainpage.jsx
@@ -63,10 +63,6 @@ const useStyles = makeStyles((theme) => ({
     // paddingBottom: theme.spacing(6),
     // },
   },
-  test: {
-    border: "1px solid red",
-    borderRadius: "5px",
-  },
 
   all: {
     padding: 0,
@@ -105,6 +101,38 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "50px",
     borderRadius: "10px",
     boxShadow: "0 0 10px -3px rgba(0,0,0 .1)",
+  },
+  contentContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontSize: "14px",
+    width: "60%",
+    height: "15px",
+    margin: "50px auto",
+    // padding: "10px",
+  },
+  content1: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  input: {
+    width: "200px",
+    height: "30px",
+    marginBottom: "15px",
+    border: "1px solid",
+    borderRadius: "3px",
+    margin: "5px",
+  },
+  button: {
+    width: "100px",
+    height: "30px",
+    marginBottom: "15px",
+    border: "1px solid",
+    borderRadius: "3px",
+    margin: "5px",
+    backgroundColor: "#8fc0a9",
   },
 }));
 
@@ -184,8 +212,8 @@ export default function Pricing() {
           <div className={classes.box}>
             <h3>Поддержка и помощь на всех этапах</h3>
             <p>
-              CRM — одна из самых дружелюбных CRM для внедрения: на каждом этапе
-              вам будут помогать наши специалисты из отдела поддержки. Мы
+              BetaCRM — одна из самых дружелюбных CRM для внедрения: на каждом
+              этапе вам будут помогать наши специалисты из отдела поддержки. Мы
               поможем настроить воронку продаж с интеграциями, автоматические и
               триггерные рассылки, дополнительные поля, виджеты онлайн записи,
               кастомные отчеты и другие важные параметры системы. Для обращения
@@ -205,12 +233,10 @@ export default function Pricing() {
           <div className={classes.box}>
             <h3>Интерактивный календарь</h3>
             <p>
-              Календарь — основной инструмент работы учебного центра. Всё как на
-              ладони: занятые и свободные аудитории, уроки, педагоги, пробные,
+              Календарь — основной инструмент работы любого бизнеса. Всё как на
+              ладони: занятые и свободные аудитории, клиенты, встречи, пробные,
               отмены, отработки. При беглом взгляде на календарь всегда можно
-              понять кто придёт на урок, у кого из них заканчивается оплата.
-              Можно легко отфильтровать по разным критериям, изменить тип
-              отображения или состав карточки.
+              понять что у вас сейчас по расписанию.
             </p>
             <img
               src="https://alfacrm.pro/images/screenshots/calendar/3.png"
@@ -219,14 +245,119 @@ export default function Pricing() {
             />
           </div>
         </div>
+
+        <div className={classes.container}>
+          <div className={classes.box}>
+            <h3>Нас выбирают лидеры в своей отрасли</h3>
+            <h5>
+              Сегодня BetaCRM пользуются более 1000 компаний. Некоторых,
+              вероятно, вы можете знать.
+            </h5>
+            <div className="imgs">
+            <img
+              src="https://alfacrm.pro/images/clients/iq007.png"
+              width="150"
+              height="70px"
+            />
+            <img
+              src="https://alfacrm.pro/images/clients/usmas.png"
+              width="150"
+              height="70px"
+            />
+            <img
+              src="https://alfacrm.pro/images/clients/menar.png"
+              width="150"
+              height="70px"
+            />
+            <img
+              src="https://alfacrm.pro/images/clients/ligarobotov.png"
+              width="150"
+              height="70px"
+            />
+            <img
+              src="https://alfacrm.pro/images/clients/smartunivercity.svg"
+              width="150"
+              height="70px"
+            />
+            <img
+              src="https://alfacrm.pro/images/clients/rs.png"
+              width="150"
+              height="70px"
+            />
+
+
+
+            <img
+              src="https://cdn-0.brodude.ru/jT8R6YZc6gRUDXH17Q-jAsbtFb74Z-VOJ6T1aI1x6WQ/rs:fill:0:0:0/g:ce/c:0:0/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDE0LzAzL2Jyb2R1ZGUucnVfMy4wMy4yMDE0X3hxaWxEdUl2dmc1ODkuanBn"
+              width="70"
+              height="70px"
+            />
+            <img
+              src="https://cdn-2.brodude.ru/JvstWGbCxRhY7pansK1qeXcHqhJG3vIzWkFGMvucZLM/rs:fill:640:407:0/g:ce:0:0/c:0:0/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDE0LzAzL2Jyb2R1ZGUucnVfNi4wMy4yMDE0Xzd5ajRHNXhQQ1ExQzEuanBn"
+              width="90"
+              height="70px"
+            />
+            <img
+              src="https://cdn-2.brodude.ru/g8nAAvpy6W4FWFEVQCqkA_ALzEBSMyJvEwGtSjubrCo/rs:fill:640:317:0/g:ce:0:0/c:0:0/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDE0LzAzL2Jyb2R1ZGUucnVfNi4wMy4yMDE0X2xsN2hkbGV1M0liNXEuanBn"
+              width="150"
+              height="70px"
+            />
+            <img
+              src="https://cdn-3.brodude.ru/Dy8U4c4y02kiIlZSUD9R22-e_Y97OhJUeOj8qqZTmpE/rs:fill:640:183:0/g:ce:0:0/c:0:0/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDE0LzAzL2Jyb2R1ZGUucnVfNi4wMy4yMDE0X2lXSElIWUJWYTJ2NVEuZ2lm"
+              width="150"
+              height="70px"
+            />
+            <img
+              src="https://cdn-2.brodude.ru/FwK8PvWzQgIn8LG5O0ZWOrZ3tcEdk_5CqwaNYhZ9Wx4/rs:fill:640:469:0/g:ce:0:0/c:0:0/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDE0LzAzL2Jyb2R1ZGUucnVfNi4wMy4yMDE0X2x5cU9pNXYxOTBkYW8uanBn"
+              width="120"
+              height="70px"
+            />
+            <img
+              src="https://cdn-0.brodude.ru/VdYuh0O0L-UnJx2zXb0K4Lj4NbhXJed65TL3-bEgJbk/rs:fill:640:479:0/g:ce:0:0/c:0:0/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDE0LzAzL2Jyb2R1ZGUucnVfNi4wMy4yMDE0X0huN0l2S0U2Y0RHcE0uanBn"
+              width="110"
+              height="70px"
+            />
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Footer */}
-      <Container maxWidth="md" component="footer" className={classes.footer}>
-        <Box mt={5}>
-          <Copyright />
-        </Box>
-      </Container>
+      {/* <Container maxWidth="md" component="footer" className={classes.footer}> */}
+      {/* <Box mt={5}> */}
+      <div className={classes.contentContainer}>
+        <div className={classes.content1}>
+          <h5>BetaCRM</h5>
+          <p>BetaCRM — CRM система для учебного центра. Sapienti sat.</p>
+        </div>
+        <div className="content2">
+          <ul class="list-inline">
+            <li>
+              <a href="#">Главная</a>
+            </li>
+            <li>
+              <a href="#">Партнеры</a>
+            </li>
+            <li>
+              <a href="#">Тарифы</a>
+            </li>
+            <li>
+              <a href="#">Интеграции</a>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.content3}>
+          <h5>Подписка на наши новости</h5>
+          <input
+            className={classes.input}
+            type="text"
+            placeholder="Введите Ваш email"
+          />
+          <button className={classes.button}>Подписаться</button>
+        </div>
+      </div>
+      {/* </Box> */}
+      {/* </Container> */}
       {/* End footer */}
     </React.Fragment>
   );
